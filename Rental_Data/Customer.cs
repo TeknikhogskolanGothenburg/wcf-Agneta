40888a4a36fa31071a3b5a762605aaf1694fb0fc
Agneta Gustafsson <agneta.kode@outlook.com>
2018-10-17 09:58:50 +0200

    customer class

--- Rental_Data/Customer.cs
+++ Rental_Data/Customer.cs
@@ -4,7 +4,20 @@ using System.Text;
 
 namespace Rental_Data
 {
-    class Customer
+    public class Customer
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
     }
 }
