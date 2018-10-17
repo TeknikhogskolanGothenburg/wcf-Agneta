a41c9448d1f204559a32faa8cddebe2c1b71036f
Slotheron <32925757+Slotheron@users.noreply.github.com>
2018-10-17 13:07:17 +0200

    spacing changes

--- Rental_Logic/Rentals.cs
+++ Rental_Logic/Rentals.cs
@@ -11,7 +11,6 @@ namespace Rental_Logic
         public List<Customer> Customers = new List<Customer>();
         public List<Booking> Bookings = new List<Booking>();
         
-   
         public void AddCar(int regNumber, string brand, int year, string model)
         {
             Car newCar = new Car() {
@@ -23,10 +22,12 @@ namespace Rental_Logic
             };
             Cars.Add(newCar);
         }
+
         public void RemoveCar(int regNumber)
         {
             Cars.RemoveAll(b => b.RegNumber == regNumber);
         }
+
         public void AddCustomer(string firstName, string lastName, string phoneNumber, string emailAddress)
         {
             Customer newCustomer = new Customer()
@@ -39,16 +40,13 @@ namespace Rental_Logic
             };
             Customers.Add(newCustomer);
         }
+
         public void RemoveCustomer(string firstName, string lastName, int id)
         {
-
             Customers.RemoveAll(c => c.FirstName == firstName && c.LastName == lastName && c.Id == id);
-
         }
 
-
         public void AddBooking(Car rentalCar, Customer renter, DateTime startTime, DateTime endTime)
-
         {
             Booking newBooking = new Booking()
             {
@@ -58,17 +56,13 @@ namespace Rental_Logic
                 StartTime = startTime,
                 EndTime = endTime,
                 IsReturned = false
-
             };
             Bookings.Add(newBooking);
-
         }
 
         public void DeleteBooking(string bookingId)
         {
             Bookings.RemoveAll(b => b.Id == bookingId);
         }
-
     }
-
  }
