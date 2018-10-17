b0199fb223612d3fe14db319080a05587667db50
Kull1z <Jocke90@hotmail.com>
2018-10-17 10:03:24 +0200

    Car class

--- Rental_Data/Car.cs
+++ Rental_Data/Car.cs
@@ -1,8 +1,15 @@
 ﻿using System;
 
-namespace Rental_Data
+namespace ClassLibrary2
 {
     public class Car
     {
+
+        private int regNumber { get; set; }
+        private string brand { get; set; }
+        private int year { get; set; }
+        private string model { get; set; }
+        private string productionYear { get; set; }
     }
+
 }
