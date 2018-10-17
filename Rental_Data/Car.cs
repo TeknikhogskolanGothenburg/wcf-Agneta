c3d0f112439e84bae9ffb89800299b90c26d29aa
Kull1z <Jocke90@hotmail.com>
2018-10-17 10:11:59 +0200

    Bool in Car class

--- Rental_Data/Car.cs
+++ Rental_Data/Car.cs
@@ -3,11 +3,12 @@
     public class Car
     {
 
-        private int Regnumber { get; set; }
+        private int RegNumber { get; set; }
         private string Brand { get; set; }
         private int Year { get; set; }
         private string Model { get; set; }
         private string ProductionYear { get; set; }
+        private bool IsRented { get; set; }
     }
 
 }
