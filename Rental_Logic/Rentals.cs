d314ebeae77dc10ca57a14f556902bb333ad7263
Agneta Gustafsson <agneta.kode@outlook.com>
2018-10-17 11:21:12 +0200

    Customer Add och Remove

--- Rental_Logic/Rentals.cs
+++ Rental_Logic/Rentals.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Rental_Data;
+using System.Linq;
 
 namespace Rental_Logic
 {
@@ -20,6 +21,24 @@ namespace Rental_Logic
                 IsRented = false
             };
             Cars.Add(newCar);
+        }
+        public void AddCustomer(string firstName, string lastName, string phoneNumber, string emailAddress)
+        {
+            Customer newCustomer = new Customer()
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                PhoneNumber = phoneNumber,
+                EmailAddress = emailAddress
+
+            };
+            Customers.Add(newCustomer);
+        }
+        public void RemoveCustomer(string firstName, string lastName, int id)
+        {
+
+            Customers.RemoveAll(c => c.FirstName == firstName && c.LastName == lastName && c.Id == id);
+
         }
     }
     
