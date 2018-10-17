b7bb20e9afc30e23709ac7ae9fd058ef92b6b979
Slotheron <32925757+Slotheron@users.noreply.github.com>
2018-10-17 10:16:42 +0200

    added booking class and fixed namespaces

---
+++ Rental_Data/Booking.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rental_Data
+{
+    public class Booking
+    {
+        Car RentalCar { get; set; }
+        Customer Renter { get; set; }
+        DateTime StartTime { get; set; }
+        DateTime EndTime { get; set; }
+    }
+}
